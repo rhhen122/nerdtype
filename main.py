42f43a416dad5b6d7912cd1eb974c8b5166ad2c3
rhhen122 <rokyhenderson222@gmail.com>
2025-05-31 16:44:14 +1200

    First Commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import random
+import os
+import time
+options = [
+    " 1. Commands\n",
+    "2. Timed Commands\n",
+    "3. Display\n",
+    "4. Help/Info\n"
+]
+help = [
+    " Commands-Commands gives you a command to Copy!\n",
+    "Timed Commands-Does the same as Commands except it shows you for a breif time\n"
+   "Displays all commands that can be ruturned\n",
+]
+words_1 = [
+    "rm -rf .config/nvim/lua",
+    "brew reinstall python3",
+    "npm install -g typescript",
+    "vim test.txt",
+    "vim $(fzf)",
+    "brew uninstall emacs"
+]
+W_options = [
+    "remove nvim with brew"
+]
+W_goals = [
+    "brew uninstall neovim"
+]
+def lose(x):
+    print(f"You Lost at: {x}")
+def win(x):
+    print(f"You Won at: {x}")
+def handle_game(x):
+    if x == 1:
+        goal = random.choice(words_1)
+        print(goal)
+        in_1 = input(": ")
+        if in_1 == goal:
+            return True
+        else:
+            return False
+    if x == 2:
+        goal = random.choice(words_1)
+        print(goal)
+        time.sleep(1)
+        os.system("clear")
+        in_2 = input(": ")
+        if in_2 == goal:
+            return True
+        else:
+            return False
+    if x == 3:
+        print(*words_1)
+        return True
+    if x == 4:
+        print(*help)
+        return True
+def handle_optionin(x):
+    if x == "1":
+        game_1 = handle_game(1)
+        if game_1 == True:
+            win("Commands")
+        else:
+            lose("Commands")
+    if x == "2":
+        game_2 = handle_game(2)
+        if game_2 == True:
+            win("Write Commands")
+        else:
+            lose("Write Commands")
+    if x == "3":
+        game_3 = handle_game(3)
+        if game_3 == True:
+            win("Display")
+        else:
+            lose("Display")
+    if x == "4":
+        game_4 = handle_game(4)
+        if game_4 == True:
+            win("Help")
+        else:
+            lose("Help")
+def handle_start():
+    print(*options)
+    optionin = input(": ")
+
+    handle_optionin(optionin)
+handle_start()
