691d0d537e1fcc4d4d9d8bb62584e841c3d8b6b9
rhhen122 <rokyhenderson222@gmail.com>
2025-07-08 00:41:58 +1200

    SO CLOSE

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ def your_game():
     # To tell Nerdtype that the player won use the: return True
     # Tot tell Nerdtype that the player lost use the: return False
     print("Something")
-your_game_status = False # This means that Nerdtype wont render your game in the menu
-your_game_name = "" # Sets your game name that needs to be used anywhere any time by Nerdtype
+    return True
+your_game_name = "Default String" # Sets your game name that needs to be used anywhere any time by Nerdtype
--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from config import your_game_name
 import random
 import os
 import time
+
 options = [
     " 1. Commands\n",
     "2. Timed Commands\n",
     "3. Display\n",
     "4. Help/Info\n",
-    "5. Keep Going!\n"
+    "5. Keep Going!\n",
+    f"6. {your_game_name} \n",
 ]
 help = [
     " Commands-Commands gives you a command to Copy!\n",
     "Timed Commands-Does the same as Commands except it shows you for a breif time\n",
-   "Displays all commands that can be ruturned\n",
+    "Displays all commands that can be ruturned\n",
 ]
 words_1 = [
     "rm -rf .config/nvim/lua",
@@ -27,18 +29,20 @@ words_1 = [
     "git commit -m 'A really cool commit message here",
     "apt update",
     "cowsay '(:'",
-    "sl"
+    "sl",
 ]
-W_options = [
-    "remove nvim with brew"
-]
-W_goals = [
-    "brew uninstall neovim"
-]
+W_options = ["remove nvim with brew"]
+W_goals = ["brew uninstall neovim"]
+
+
 def lose(x):
     print(f"You Lost at: {x}")
+
+
 def win(x):
     print(f"You Won at: {x}")
+
+
 def handle_game(x):
     if x == 1:
         goal = random.choice(words_1)
@@ -71,7 +75,7 @@ def handle_game(x):
             print(goal)
             time.sleep(1)
             os.system("clear")
-            if y == True:
+            if y:
                 win("Keep Going!")
             else:
                 lose("Keep Going!")
@@ -80,40 +84,54 @@ def handle_game(x):
                 y = True
             else:
                 y = False
+    if x == 6:
+        your_game()
+
+
 def handle_optionin(x):
     if x == "1":
         game_1 = handle_game(1)
-        if game_1 == True:
+        if game_1:
             win("Commands")
         else:
             lose("Commands")
     if x == "2":
         game_2 = handle_game(2)
-        if game_2 == True:
+        if game_2:
             win("Write Commands")
         else:
             lose("Write Commands")
     if x == "3":
         game_3 = handle_game(3)
-        if game_3 == True:
+        if game_3:
             win("Display")
         else:
             lose("Display")
     if x == "4":
         game_4 = handle_game(4)
-        if game_4 == True:
+        if game_4:
             win("Help")
         else:
             lose("Help")
     if x == "5":
         game_5 = handle_game(5)
-        if game_5 == True:
+        if game_5:
             win("Keep Going!")
         else:
             lose("Keep Going!")
+    if x == "6":
+        game_6 = handle_game(6)
+        if game_6:
+            win(your_game_name)
+        else:
+            lose(your_game_name)
+
+
 def handle_start():
     print(*options)
     optionin = input(": ")
 
     handle_optionin(optionin)
+
+
 handle_start()
